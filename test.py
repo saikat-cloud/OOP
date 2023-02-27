75cccdf11e52fc007d954dc8321b30f6c1c6fa56
saikat <saikat.mondal.dev@gmail.com>
2023-02-27 15:30:18 +0600

    test file added

---
+++ test.py
@@ -0,0 +1,23 @@
+
+class Test:
+
+    # def __init__(self,):
+    #     self.add = add
+    #     self.sub = sub
+
+    def add(self,x,y):
+        result = x+ y
+        print(result) 
+
+    def sub(self,x,y):
+        result = x - y
+        print(result)
+    
+
+obje = Test()
+
+obje.add(2,3)
+obje.sub(3,4)
+
+
+
